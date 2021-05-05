d5077ee4b62f3a63a971332dcccb9fac4279858b
juarez-gonza <gmjuareztello@gmail.com>
2021-05-04 22:41:00 -0300

    recursive sdt

--- 3/ll1_predictive.py
+++ 3/ll1_predictive.py
@@ -39,11 +39,11 @@ class Token(Grammar_Symbol):
         super().__init__(name, value)
 
     @staticmethod
-    def gen_token(c):
+    def gen(c):
         #if Token.token_patterns[0].match(c):
         #    return Token(TERM.ID.name, TERM.ID.value, c)
         if Token.token_patterns[0].match(c):
-            return Token(TERM.NUM.name, TERM.NUM.value, c)
+            return Token(TERM.NUM.name, TERM.NUM.value, float(c))
         elif Token.token_patterns[1].match(c):
             return Token(TERM.SUM.name, TERM.SUM.value)
         elif Token.token_patterns[2].match(c):
@@ -57,8 +57,8 @@ class Token(Grammar_Symbol):
 
 class Token_Stream():
     def __init__(self, inp):
-        self.token_stream = [Token.gen_token(c) for c in inp]
-        self.token_stream.append(Token.gen_token("$"))
+        self.token_stream = [Token.gen(c) for c in inp]
+        self.token_stream.append(Token.gen("$"))
         self.idx = 0
 
     def get_next_token(self):
@@ -77,6 +77,90 @@ class NTERM(enum.Enum):
     T = 2
     T_ = 3
     F = 4
+
+class Non_Term(Grammar_Symbol):
+    def __init__(self, name, value):
+        self.leaves = []
+        self.interiors = []
+        super().__init__(name, value)
+
+    def append_interior(self, childnode):
+        self.interiors.append(childnode)
+
+    def append_leaf(self, childnode):
+        self.leaves.append(childnode)
+
+    def walk_arithmetic(self, inh=None):
+        pass
+
+    @staticmethod
+    def gen(nterm):
+        if nterm.name == NTERM.E.name:
+            return E(nterm.name, nterm.value)
+        elif nterm.name == NTERM.E_.name:
+            return E_(nterm.name, nterm.value)
+        elif nterm.name == NTERM.T.name:
+            return T(nterm.name, nterm.value)
+        elif nterm.name == NTERM.T_.name:
+            return T_(nterm.name, nterm.value)
+        elif nterm.name == NTERM.F.name:
+            return F(nterm.name, nterm.value)
+        else:
+            raise ValueError("Cannot generate non terminal @ Non_Term.gen(), no match for nterm")
+
+# E  -> TE'
+class E(Non_Term):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    def walk_arithmetic(self, inh=None):
+        val_T = self.interiors[0].walk_arithmetic()
+        val_E_ = self.interiors[1].walk_arithmetic(val_T)
+        return val_E_
+
+# E' -> +TE | e
+class E_(Non_Term):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    def walk_arithmetic(self, inh=None):
+        if not self.interiors:
+            return inh
+        val_T = self.interiors[0].walk_arithmetic()
+        val_E_ = self.interiors[1].walk_arithmetic(inh + val_T)
+        return val_E_
+
+# T -> FT'
+class T(Non_Term):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    def walk_arithmetic(self, inh=None):
+        val_F = self.interiors[0].walk_arithmetic()
+        val_T_ = self.interiors[1].walk_arithmetic(val_F)
+        return val_T_
+
+# T' -> *FT' | e
+class T_(Non_Term):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    def walk_arithmetic(self, inh=None):
+        if not self.leaves and not self.interiors:
+            return inh
+        val_F = self.interiors[0].walk_arithmetic()
+        val_T_ = self.interiors[1].walk_arithmetic(inh * val_F)
+        return val_T_
+
+# F -> num | (E)
+class F(Non_Term):
+    def __init__(self, name, value):
+        super().__init__(name, value)
+
+    def walk_arithmetic(self, inh=None):
+        if self.leaves[0].name == TERM.NUM.name:
+            return self.leaves[0].attr
+        return self.interiors[0].walk_arithmetic()
 
 class Stack():
     def __init__(self):
@@ -110,44 +194,65 @@ class Parser():
             [[TERM.NUM],            [],                             [],                                         [TERM.O_PAREN, NTERM.E, TERM.C_PAREN],      [],                 []],
         ]
 
-        self.s_sym = NTERM.E
-        self.end_sym = Token.gen_token("$")
+        self.end_sym = Token.gen("$")
+        self.root = Non_Term.gen(NTERM.E)
+
         self.stack = Stack()
         self.stack.push(self.end_sym)
-        self.stack.push(self.s_sym)
+        self.stack.push(self.root)
 
     def predictive(self):
-        X = self.stack.top()
-        a = self.token_stream.get_next_token()
+        self.curr = self.stack.top()
+        self.prev = self.curr
+        self.a = self.token_stream.get_next_token()
+        while self.curr.name != self.end_sym.name:
 
-        while X.name != self.end_sym.name:
-            if X.name == a.name:
-                print("\t====Match de token X = %s (%d), a = %s (%d)====" % (X.name, X.value, a.name, a.value))
+            if self.curr.name == self.a.name:
                 self.stack.pop()
-                a = self.token_stream.get_next_token()
-            elif isinstance(X, Token):
-                raise ValueError("Un token no debería llegar hasta aquí. X: name=%s value=%d attr=%s" % (X.name, X.value, X.attr))
-            elif not self.ptable[X.value][a.value]:
-                raise ValueError("No hay entrada para X = %s (%d), a = %s (%d)" % (X.name, X.value, a.name, a.value))
+                self.prev.append_leaf(self.a)
+                self.a = self.token_stream.get_next_token()
+            elif isinstance(self.curr, Token):
+                raise ValueError("Un token no debería llegar hasta aquí. X: name=%s value=%d attr=%s" % (self.curr.name, self.curr.value, self.curr.attr))
+            elif not self.ptable[self.curr.value][self.a.value]:
+                raise ValueError("No hay entrada para X = %s (%d), a = %s (%d)" % (self.curr.name, self.curr.value, self.a.name, self.a.value))
             else:
-                print("Match de entrada para X = %s (%d), a = %s (%d)" % (X.name, X.value, a.name, a.value))
-                production = self.ptable[X.value][a.value]
+                production = self.ptable[self.curr.value][self.a.value]
+                self.creatnreplace_ints(production)
+                self.append_ints(production)
                 self.stack.pop()
                 self.push_production(production)
-            X = self.stack.top()
-        print("\t====Match de símbolo de finalización X = %s, a = %s====" % (X.name, a.name))
+
+            self.prev = self.curr
+            self.curr = self.stack.top()
+        return self.root
+
+    def append_ints(self, production):
+        for i in range(len(production)):
+            g_sym = production[i]
+            if isinstance(g_sym, TERM):
+                continue
+            self.curr.append_interior(g_sym)
+
+    def creatnreplace_ints(self, production):
+        for i in range(len(production)):
+            g_sym = production[i]
+            if isinstance(g_sym, TERM):
+                continue
+            interior = Non_Term.gen(g_sym)
+            production[i] = interior
 
     def push_production(self, production):
-        if production[0] == TERM.EMPTY:
+        if production[0].name == TERM.EMPTY.name:
             return
         for i in range(len(production) - 1, -1, -1):
-            grammar_sym = production[i]
-            self.stack.push(grammar_sym)
-
+            g_sym = production[i]
+            self.stack.push(g_sym)
 
 if __name__ == "__main__":
     #inp = input()
-    inp = "7+3*4"
+    inp = "(7+3)*4"
     token_stream = Token_Stream(inp)
     parser = Parser(token_stream)
-    parser.predictive()
+    parsetree = parser.predictive()
+    res = parsetree.walk_arithmetic()
+    print(res)
